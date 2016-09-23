c72a3663e938c1a25ed280c9d05ef8df2cd5f58e
rjoshi6 <rahulrjoshi225@gmail.com>
2016-09-23 12:16:54 -0400

    Input VAlidation
    Done on

---
+++ MainController.js
@@ -0,0 +1,8 @@
+app.controller('MainController', ['$scope', function($scope) { 				//controller
+ $scope.firstName = "";
+ $scope.lastName = "";
+ $scope.states = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME",
+"MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN",
+"TX","UT","VT","VA", "WA","WV","WI","WY"];
+
+}]);
